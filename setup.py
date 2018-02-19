d838c3c6476dbccfb41fd23d9305fabcd0c7912d
jarbasai <jarbasai@mailfence.com>
2018-02-19 20:31:40 +0000

    pip

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 setup(
     name='deep_dreamer',
-    version='0.01',
+    version='0.011',
     packages=['src'],
-    url='',
+    url='https://github.com/JarbasAl/deep_dreamer',
     license='MIT',
     author='jarbasAi',
     author_email='jarbasai@mailfence.com',
+    download_url = 'https://github.com/JarbasAl/deep_dreamer/archive/0.011.tar.gz',
     description='wrapper for https://github.com/ProGamerGov/Protobuf-Dreamer'
 )
